b96c7a60f83a9cf7c1421ce85ae1aba5b638bd4f
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-04-25 23:12:10 +0100

    Dice now shows correct number of dots according to roll.

--- js/index.js
+++ js/index.js
@@ -1,42 +1,100 @@
-var off // Your code here
+// Your code here
 function diceRandom(num) {
-    return Math.floor(Math.random() * Math.floor(num));
+    var theRoll = Math.floor(Math.random() * (num) + 1);
+    if (theRoll > 0) {return theRoll;}
+
 }
 
-var diceRoll = diceRandom();r
+var diceRoll = diceRandom(6);
 
-console.log(diceRandom(6));
+console.log(diceRoll);
 
-var diceSquareOne = document.getElementById('one')
-var diceSquareTwo = document.getElementById('two')
-var diceSquareThree = document.getElementById('three')
-var diceSquareFour = document.getElementById('four')
-var diceSquareFive = document.getElementById('five')
-var diceSquareSix = document.getElementById('six')
-var diceSquareSeven = document.getElementById('seven')
-var diceSquareEight = document.getElementById('eight')
-var diceSquareNine = document.getElementById('nine')
+var diceSquareOne = document.getElementById('one');
+var diceSquareTwo = document.getElementById('two');
+var diceSquareThree = document.getElementById('three');
+var diceSquareFour = document.getElementById('four');
+var diceSquareFive = document.getElementById('five');
+var diceSquareSix = document.getElementById('six');
+var diceSquareSeven = document.getElementById('seven');
+var diceSquareEight = document.getElementById('eight');
+var diceSquareNine = document.getElementById('nine');
 
-var offOne = diceDisplaySquareOne.style.display = "none";
-var offTwo = diceSquareTwo.style.display = "none";
-var offThree = diceSquareThree.style.display = "none";
-var offFour = diceSquareFour.style.display = "none";
-var offFive = diceSquareFive.style.display = "none";
-var offSix = diceSquareSix.style.display = "none";
-var offSeven = diceSquareSeven.style.display = "none";
-var offEight = diceSquareEight.style.display = "none";
-var offNine = diceSquareNine.style.display = "none";
+var turnOffDot = new function () {
+  this.One = function () {diceSquareOne.style.display = "none";}
+  this.Two = function () {diceSquareTwo.style.display = "none";}
+  this.Three = function () {diceSquareThree.style.display = "none";}
+  this.Four = function () {diceSquareFour.style.display = "none";}
+  this.Five = function () {diceSquareFive.style.display = "none";}
+  this.Six = function () {diceSquareSix.style.display = "none";}
+  this.Seven = function () {diceSquareSeven.style.display = "none";}
+  this.Eight = function () {diceSquareEight.style.display = "none";}
+  this.Nine = function () {diceSquareNine.style.display = "none";}
+}
+
+function showOne () {
+  turnOffDot.One();
+  turnOffDot.Two();
+  turnOffDot.Three();
+  turnOffDot.Four();
+  turnOffDot.Six();
+  turnOffDot.Seven();
+  turnOffDot.Eight();
+  turnOffDot.Nine();
+}
+
+function showTwo () {
+  turnOffDot.One();
+  turnOffDot.Two();
+  turnOffDot.Three();
+  turnOffDot.Five();
+  turnOffDot.Seven();
+  turnOffDot.Eight();
+  turnOffDot.Nine();
+}
+
+function showThree() {
+  turnOffDot.One();
+  turnOffDot.Two();
+  turnOffDot.Three();
+  turnOffDot.Seven();
+  turnOffDot.Eight();
+  turnOffDot.Nine();
+}
+function showFour() {
+  turnOffDot.Two();
+  turnOffDot.Four();
+  turnOffDot.Five();
+  turnOffDot.Six();
+  turnOffDot.Eight();
+}
+function showFive() {
+  turnOffDot.Two();
+  turnOffDot.Four();
+  turnOffDot.Six();
+  turnOffDot.Eight();
+}
+function showSix() {
+  turnOffDot.Two();
+  turnOffDot.Five();
+  turnOffDot.Eight();
+}
 
 
-var dice = new function (d1, d2, d3, d4, d5, d6, d7, d8, d9) {
-  this.one = if (diceRoll == 1) {
-    d1;
-    d2;
-    d3;
-    d4;
-    d6;
-    d7;
-    d8;
-    d9;
-  }
+if (diceRoll === 1) {
+  showOne();
+}
+else if (diceRoll === 2) {
+  showTwo();
+}
+else if (diceRoll === 3) {
+  showThree();
+}
+else if (diceRoll === 4) {
+  showFour();
+}
+else if (diceRoll === 5) {
+  showFive();
+}
+else if (diceRoll === 6) {
+  showSix();
 }
